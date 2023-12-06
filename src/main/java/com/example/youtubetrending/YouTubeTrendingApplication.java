2a3e5bcc2526eba1b6381184b1bfa7f94a29d73b
lexash707 <lexashart@gmail.com>
2023-12-06 09:44:13 +0100

    initial commit

---
+++ src/main/java/com/example/youtubetrending/YouTubeTrendingApplication.java
@@ -0,0 +1,13 @@
+package com.example.youtubetrending;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouTubeTrendingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YouTubeTrendingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/youtubetrending/YouTubeTrendingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.youtubetrending;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouTubeTrendingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

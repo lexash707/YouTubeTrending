d1b2bbb3c0f6008cc05e17c716e8824a993b6de0
lexash707 <lexashart@gmail.com>
2023-12-06 09:48:53 +0100

    springn't

--- src/main/java/com/example/youtubetrending/YouTubeTrendingApplication.java
+++ src/main/java/com/example/youtubetrending/YouTubeTrendingApplication.java
@@ -1,13 +1,12 @@
 package com.example.youtubetrending;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class YouTubeTrendingApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(YouTubeTrendingApplication.class, args);
+
+
+
+
     }
 
 }
